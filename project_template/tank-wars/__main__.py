87c191bc7af184945332d747fa111befbd53ccbd
Reed <reed.hunsaker@gmail.com>
2021-03-03 17:06:48 -0700

    ground in progress

--- project_template/tank-wars/__main__.py
+++ project_template/tank-wars/__main__.py
@@ -1,13 +1,24 @@
 # program entry point
 
 import arcade
+import random
+import game.constants
 from game.output_service import Output_service
 from game.director import Director
+from game.actor import Actor
+from game.point import Point
 
 def main():
    # create the cast {key: tag, value: list}
     cast = {}
     
+    cast["ground"] = []
+    for x in range(0, game.constants.X_CONSTANT):
+        for y in range(0, 150):
+            y = random.randint(0, 150)
+            position = Point(x,y)
+            ground = Actor()
+            ground.set_position(position)
     # create the script {key: tag, value: list}
     script = {}
 
