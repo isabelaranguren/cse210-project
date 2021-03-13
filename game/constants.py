59e57b3480a98ad1b7970f388e2da0ba323f13f3
aalund2013 <ran08001@byui.edu>
2021-03-13 08:26:27 -0700

    file paths work!

--- game/constants.py
+++ game/constants.py
@@ -1,30 +1,31 @@
-import os
 from pathlib import Path
-cwd = Path.cwd()
-assets = cwd / 'assets'
 home = Path.home()
+assets: Path = Path(__file__).parent / 'assets'
+
 #frame
 X_CONSTANT = 800
 Y_CONSTANT = 600
 SCREEN_TITLE = "TANK WARS"
+
 #sprites
 #ground
 GROUND_SPRITE = f":resources:images/tiles/dirtRight.png"
 GROUND_SCALE = 1
 GROUND_Y = 32
+
 #Tanks
 TANK_Y = 125
 TANK_SCALE = 0.85
 #Player 1
 PLAYER1_X = 150
 PLAYER1_SPRITE = assets / 'tank-pack' / 'tanks_tankGrey1.png'
-# PLAYER1_SPRITE = f"{cwd}/assets/tank-pack/tanks_tankGrey1.png"
+
 #player 2
 PLAYER2_SPRITE = assets / 'tank-pack' / 'tanks_tankGreen1.png'
-# PLAYER2_SPRITE = f"{cwd}/assets/tank-pack/tanks_tankGreen1.png"
 PLAYER2_X = 600
+
 #bullet
 BULLET_SPRITE = assets/ 'tank-pack' / 'tank_bulletFly4.png'
-# BULLET_SPRITE = f"{cwd}assets/tank-pack/tank_bulletFly4.png"
+
 #time
 FRAME_LENGTH = 0.1
