c9ac87d8a0c914a0960e6da6419a65ef8135822d
Reed <reed.hunsaker@gmail.com>
2021-03-13 15:00:41 -0700

    Pew pew

--- game/bullet.py
+++ game/bullet.py
@@ -1,5 +1,6 @@
 import arcade
 import game.constants as constants
+import math
 
 class BulletSprite(arcade.Sprite):
     def __init__(self):
@@ -7,21 +8,26 @@ class BulletSprite(arcade.Sprite):
 
         self.texture = arcade.load_texture(file_name= constants.BULLET_SPRITE)
         self.scale = constants.BULLET_SCALE
-
+        self.speed = constants.BULLET_SPEED
         self.tank_fire = arcade.load_sound(constants.TANK_FIRE)
+
 
 
 class Bullet:
     def __init__(self):
         super().__init__()
-        self.bullet = BulletSprite()
-        # self.bullet_sprite_list = arcade.SpriteList()
-        # self.bullet_sprite_list.append(self.bullet)
-
-    def shoot_bullet(self,tank_x, tank_y):
-        self.bullet.center_x = tank_x
-        self.bullet.center_y = tank_y
-        self.bullet.velocity = constants.BULLET_INITIAL_VELOCITY
         self.bullet_sprite_list = arcade.SpriteList()
+        
+    def shoot_bullet(self,tank_x, tank_y, tank_angle):
+        self.bullet = BulletSprite()
+        self.bullet.change_y = -math.cos(math.radians(tank_angle)) * self.bullet.speed
+        self.bullet.change_x = math.sin(math.radians(tank_angle)) * self.bullet.speed
+        self.bullet.center_x = tank_x + (self.bullet.change_x * 9)
+        self.bullet.center_y = tank_y + (self.bullet.change_y * 9)
+        self.bullet.angle = math.degrees(math.atan2(self.bullet.change_y, self.bullet.change_x))
+        # self.bullet.velocity = constants.BULLET_INITIAL_VELOCITY
         self.bullet_sprite_list.append(self.bullet)
         arcade.play_sound(self.bullet.tank_fire)
+        # Convert angle in degrees to radians.
+        self.bullet.update()
+        
