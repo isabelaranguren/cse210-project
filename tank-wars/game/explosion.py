04e23e22b7680205eb2559ec3d711aa5ae234c48
aalund2013 <ran08001@byui.edu>
2021-03-30 19:46:11 -0600

    explosion comments added

--- tank-wars/game/explosion.py
+++ tank-wars/game/explosion.py
@@ -2,13 +2,30 @@ import arcade
 import game.constants as constants
 
 class Explosion(arcade.Sprite):
+    """ Controls the progression of the explosion
+    Stereotype:
+        Information Holder
+    Attributes:
+        current_texture (None): declares current texture variable
+        textures (None): declares texture list variable
+    Contributors:
+        Adrianna Lund
+    """
     def __init__(self, texture_list):
+        """ Initializes variables for the Explosion class
+        Contributors:
+            Adrianna Lund
+        """
         super().__init__()
         self.current_texture = 0
         self.textures = texture_list
         
     
     def update(self):
+        """ Progresses through the list of images for the explosion, then removes them from the sprite lists
+        Contributors:
+            Adrianna Lund
+        """
         self.current_texture +=1
         if self.current_texture < len(self.textures):
             self.set_texture(self.current_texture)
